05ce51b0bfad34a0aad3b57ee20b42503561ff98
yaoweizhang <512094714@qq.com>
2022-08-10 10:29:14 +0800

    add cityscpse to ACDC

---
+++ configs/_base_/datasets/uda_cityscapes_to_acdc_night_512x512.py
@@ -0,0 +1,88 @@
+# dataset settings
+dataset_type = 'CityscapesDataset'
+data_root = 'data/cityscapes/'
+img_norm_cfg = dict(
+    mean=[58.413, 45.7, 34.42], std=[41.507, 37.797, 31.376], to_rgb=True)
+crop_size = (512, 512)
+#gta_train_pipeline = [
+#    dict(type='LoadImageFromFile'),
+#    dict(type='LoadAnnotations'),
+#    dict(type='Resize', img_scale=(1280, 720)),
+#    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+#    dict(type='RandomFlip', prob=0.5),
+#    # dict(type='PhotoMetricDistortion'),  # is applied later in dacs.py
+#    dict(type='Normalize', **img_norm_cfg),
+#    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+#    dict(type='DefaultFormatBundle'),
+#    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+#]
+cityscapes_train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=(1024, 512)),
+    dict(type='RandomCrop', crop_size=crop_size),
+    dict(type='RandomFlip', prob=0.5),
+    # dict(type='PhotoMetricDistortion'),  # is applied later in dacs.py
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+acdc_night_train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=(960, 512)),
+    dict(type='RandomCrop', crop_size=crop_size),
+    dict(type='RandomFlip', prob=0.5),
+    # dict(type='PhotoMetricDistortion'),  # is applied later in dacs.py
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(960, 512),
+        # MultiScaleFlipAug is disabled by not providing img_ratios and
+        # setting flip=False
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=4,
+    train=dict(
+        type='UDADataset',
+        source=dict(
+            type='CityscapesDataset',
+            data_root='data/cityscapes/',
+            img_dir='leftImg8bit/train',
+            ann_dir='gtFine/train',
+            pipeline=cityscapes_train_pipeline),
+        target=dict(
+            type='ACDCDataset',
+            data_root='data/ACDC/',
+            img_dir='rgb_anon/night/train',
+            ann_dir='gt/night/train',
+            pipeline=acdc_night_train_pipeline)),
+    val=dict(
+        type='ACDCDataset',
+        data_root='data/ACDC/',
+        img_dir='rgb_anon/night/val',
+        ann_dir='gt/night/val',
+        pipeline=test_pipeline),
+    test=dict(
+        type='ACDCDataset',
+        data_root='data/ACDC/',
+        img_dir='rgb_anon/night/test',
+        #ann_dir='gt/night/test',
+        pipeline=test_pipeline))
